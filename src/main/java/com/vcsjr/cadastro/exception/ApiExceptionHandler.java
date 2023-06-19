44a8bd47303440f8daab092538b113538fbd4ad5
Valmir Correa <valmircontato96@gmail.com>
2023-06-19 16:05:04 -0300

    feature: add exception handler and tests

--- src/main/java/com/vcsjr/cadastro/exception/ApiExceptionHandler.java
+++ src/main/java/com/vcsjr/cadastro/exception/ApiExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.vcsjr.cadastro.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -11,4 +17,16 @@ public class ApiExceptionHandler {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
+    @ExceptionHandler({ ConstraintViolationException.class })
+    public ResponseEntity<String> handleHttpClientError(ConstraintViolationException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    public ResponseEntity<List<String>> handleHttpClientError(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
--- src/test/java/com/vcsjr/cadastro/exception/ApiExceptionHandlerTest.java
+++ src/test/java/com/vcsjr/cadastro/exception/ApiExceptionHandlerTest.java
@@ -1,22 +1,39 @@
 package com.vcsjr.cadastro.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApiExceptionHandlerTest {
+
+    private ApiExceptionHandler apiExceptionHandler = new ApiExceptionHandler();
 
     @Test
     void testHandleIllegalArgumentException() {
         String errorMessage = "Invalid argument";
         IllegalArgumentException exception = new IllegalArgumentException(errorMessage);
 
-        ApiExceptionHandler exceptionHandler = new ApiExceptionHandler();
-        ResponseEntity<String> response = exceptionHandler.handleIllegalArgumentException(exception);
+        ResponseEntity<String> response = apiExceptionHandler.handleIllegalArgumentException(exception);
 
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
         assertEquals(errorMessage, response.getBody());
+    }
+
+    @Test
+    void handleConstraintViolationException() {
+        ConstraintViolationException exception = new ConstraintViolationException(Set.of());
+        ResponseEntity<String> responseEntity = apiExceptionHandler.handleHttpClientError(exception);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("", responseEntity.getBody());
     }
 }
